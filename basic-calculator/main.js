87357d0ee89de11358f977798cdd5fb2a9cf4cdb
Salvador Perez <salvadorperezm115@gmail.com>
2022-04-13 18:40:32 -0600

    basic function done, missing design

--- basic-calculator/main.js
+++ basic-calculator/main.js
@@ -2,25 +2,84 @@ const buttonsContainer = document.querySelector(".buttons-container");
 const resultsContainer = document.querySelector(".results-container");
 
 let num = 0;
+let newNumber = 0;
+let currentOperation = "";
+
+function equalsButtonOperation() {
+    switch (currentOperation) {
+        case "sum":
+            newNumber = Number(resultsContainer.textContent);
+            num += newNumber;
+            resultsContainer.textContent = num;
+            break;
+        
+        case "minus":
+            newNumber = Number(resultsContainer.textContent);
+            num -= newNumber;
+            resultsContainer.textContent = num;
+            break;
+        
+        case "mult":
+            newNumber = Number(resultsContainer.textContent);
+            num *= newNumber;
+            resultsContainer.textContent = num;
+            break;
+        
+        case "divide":
+            newNumber = Number(resultsContainer.textContent);
+
+            if (newNumber === 0) {
+                resultsContainer.textContent = "Error";
+            } else {
+                num /= newNumber;
+                resultsContainer.textContent = num;
+            }
+
+            break;
+    }
+}
 
 buttonsContainer.addEventListener("click", (e) => {
     const targetAttribute = e.target.getAttribute("data");
-    
-    switch(targetAttribute) {
+
+    switch (targetAttribute) {
         case "clear":
-            resultsContainer.textContent = "";
             num = 0;
+            newNumber = 0;
+            currentOperation = "";
+            resultsContainer.textContent = "";
             break;
+
         case "input":
             resultsContainer.textContent += e.target.textContent;
             break;
+
         case "sum":
-            num += Number(resultsContainer.textContent);
-            alert(num);
+            num = Number(resultsContainer.textContent);
+            currentOperation = targetAttribute;
             resultsContainer.textContent = "";
             break;
+
+        case "minus":
+            num = Number(resultsContainer.textContent);
+            currentOperation = targetAttribute;
+            resultsContainer.textContent = "";
+            break;
+
+        case "mult":
+            num = Number(resultsContainer.textContent);
+            currentOperation = targetAttribute;
+            resultsContainer.textContent = "";
+            break;
+        
+        case "divide":
+            num = Number(resultsContainer.textContent);
+            currentOperation = targetAttribute;
+            resultsContainer.textContent = "";
+            break;
+
         case "equals":
-            resultsContainer.textContent = num;
+            equalsButtonOperation();
             break;
     }
 });
