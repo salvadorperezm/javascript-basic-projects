1f17f91758f0a5b06ee35d86734509120aabe7b9
Salvador Perez <salvadorperezm115@gmail.com>
2022-05-01 18:12:11 -0600

    updating dev branch

--- basic-calculator/main.js
+++ basic-calculator/main.js
@@ -2,15 +2,56 @@ const calcDisplay = document.querySelector(".calculator__display");
 const calcKeys = document.querySelector(".calculator__keys");
 
 let currentOperation = "";
+let baseNumber = 0;
+let operationNumber = 0;
+let operationCounter = 0;
+let storedOperation = "";
+
+function clearData() {
+    calcDisplay.textContent = "0";
+    currentOperation = "";
+    baseNumber = 0;
+    operationNumber = 0;
+    operationCounter = 0;
+    storedOperation = "";
+}
 
 function inputNumberHandler(e) {
-    if (calcDisplay.textContent === "0") {
+    if (calcDisplay.textContent === "0" || calcDisplay.textContent === "Error") {
         calcDisplay.textContent = e.target.textContent;
     } else {
         calcDisplay.textContent += e.target.textContent;
     }
 }
 
+function operationHandler(baseNumber, storedOperation, operationNumber) {
+    switch (storedOperation) {
+        case "sum":
+            calcDisplay.textContent = baseNumber + operationNumber;
+            currentOperation = "";
+            break;
+        
+        case "substraction":
+            calcDisplay.textContent = baseNumber - operationNumber;
+            currentOperation = "";
+            break;
+
+        case "multiplication":
+            calcDisplay.textContent = baseNumber * operationNumber;
+            currentOperation = "";
+            break;
+        
+        case "division":
+            if (operationNumber === 0) {
+                calcDisplay.textContent = "Error";
+                currentOperation = "";
+            } else {
+                calcDisplay.textContent = baseNumber / operationNumber;
+                currentOperation = "";
+            }
+            break;
+    }
+}
 
 calcKeys.addEventListener("click", (e) => {
     const keyAction = e.target.getAttribute("data-action");
@@ -21,23 +62,40 @@ calcKeys.addEventListener("click", (e) => {
             break;
         
         case "clear":
-            calcDisplay.textContent = "0";
-            currentOperation = "";
-            n1 = 0;
-            n2 = 0;
-            operationCounter = 0;
+            clearData();
             break;
 
         case "calculate":
-            console.log("you hit the calculate button");
+            console.log(`Base number: ${baseNumber}`);
+            console.log(`Current operation: ${storedOperation}`);
+            console.log(`Operation number: ${Number(calcDisplay.textContent)}`);
+            operationHandler(baseNumber, storedOperation, Number(calcDisplay.textContent));
             break;
 
         case "number":
-            inputNumberHandler(e);
+            if (operationCounter === 0) {
+                inputNumberHandler(e);
+            } else {
+                calcDisplay.textContent = "0";
+                operationCounter--;
+                inputNumberHandler(e);
+            }
             break;
-        
+
         default:
-            console.log(`Current operation: ${keyAction}`);
+            if (currentOperation === "") {
+                baseNumber = Number(calcDisplay.textContent);
+                currentOperation = keyAction;
+                storedOperation = keyAction;
+                operationCounter++;
+            } else {
+                // if there's an active operation, it must complete first
+                console.log(`Base number: ${baseNumber}`);
+                console.log(`Current operation: ${storedOperation}`);
+                console.log(`Operation number: ${Number(calcDisplay.textContent)}`);
+                operationCounter--;
+            }
+
             break;
     }
 
